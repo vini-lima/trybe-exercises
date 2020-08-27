1b15d47d9fc4d76788b10c838bb92881640cf9dc
vini-lima <vini20.henrique@gmail.com>
2020-08-27 19:08:35 -0300

    Exercise 3

---
+++ bloco_4/dia_1/exercise3.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 3;
+let c = 5;
+
+if (a > b && a > c) {
+    console.log('O maior número é o ' + a);
+} else if (b > a && b > c) {
+    console.log('O maior número é o ' + b);
+} else {
+    console.log('O maior número é o ' + c);
+}
