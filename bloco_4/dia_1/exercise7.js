37171fbcd5f266c625d3ea5f8cb7775910777a6d
vini-lima <vini20.henrique@gmail.com>
2020-08-28 08:57:22 -0300

    Exercise 7

---
+++ bloco_4/dia_1/exercise7.js
@@ -0,0 +1,17 @@
+let nota = 54;
+
+if (nota >= 90) {
+    console.log('Você tirou a nota A');
+} else if (nota >= 80) {
+    console.log('Você tirou a nota B');
+} else if (nota >= 70) {
+    console.log('Você tirou a nota C');
+} else if (nota >= 60) {
+    console.log('Você tirou a nota D');
+} else if (nota >= 50) {
+    console.log('Você tirou a nota E');
+} else if (nota < 50) {
+    console.log('Você tirou a nota F');
+} else {
+    console.log('Erro: Nota Inválida');
+}    
