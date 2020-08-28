7f29398f10a9fbd53e13d2da52293d2ba535a773
vini-lima <vini20.henrique@gmail.com>
2020-08-28 09:08:16 -0300

    exercise 9 e Correção do 8

--- bloco_4/dia_1/exercise9.js
+++ bloco_4/dia_1/exercise9.js
@@ -1,8 +1,8 @@
-let a = 1;
-let b = 3;
+let a = 2;
+let b = 4;
 let c = 6;
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
     console.log('True');
 } else {
     console.log('False');
