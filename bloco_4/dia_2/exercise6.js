b4f56b038ca6d7003cea11fceacdede4446cbb9e
vini-lima <vini20.henrique@gmail.com>
2020-08-28 15:36:03 -0300

    Exercise 6

---
+++ bloco_4/dia_2/exercise6.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let valoresImpares = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] % 2 !== 0) {
+        valoresImpares += 1;
+    } 
+}
+
+if (valoresImpares === 0) {
+    console.log("Nenhum valor ímpar encontrado");
+} else {
+    console.log(`Existem ${valoresImpares} valores ímpares!`);
+}
