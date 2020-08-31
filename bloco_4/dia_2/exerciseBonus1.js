9498f78df82aaf9d92b41a0bfa6888f3dccc315f
vini-lima <vini20.henrique@gmail.com>
2020-08-31 15:09:39 -0300

    Exercise Bonus 1 Atualizado

--- bloco_4/dia_2/exerciseBonus1.js
+++ bloco_4/dia_2/exerciseBonus1.js
@@ -1,7 +1,7 @@
 let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 
-for (let i = 0; i < numbers.length; i += 1) {
-    for (let j = 0; j < (numbers.length - 1); j += 1) {
+for (let i = 1; i < numbers.length; i += 1) {
+    for (let j = 0; j < i; j += 1) {
         if (numbers[i] < numbers[j]) {
             let position = numbers[i];
       
